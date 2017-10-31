67b7ad58512508ce5ca5e177b3d987ba2b2f324c
Neverov Nikolay <nikolay_neverov@adoriasoft.com>
2017-10-31 22:42:46 +0300

    добавлен веб

---
+++ Web/Controllers/HomeController.cs
@@ -0,0 +1,138 @@
+﻿using FuzzySearch;
+using NetOffice.ExcelApi;
+using NLog;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        private const string FILE_KEY = "excelFile";
+        private const string FIRST_ROW_INDEX_KEY = "firstRowIndex";
+        private const string LAST_ROW_INDEX_KEY = "lastRowIndex";
+        private const string COLUMN_INDEX_KEY = "columnIndex";
+        private const string FULL_RANGE_VALUES_KEY = "fullRangeValues";
+
+        // TODO config
+        private const double fuzzyness = 0.7;
+        private const double autoFuzzyness = 0.9;
+
+        private readonly Logger _logger = LogManager.GetCurrentClassLogger();
+        private readonly Fuzzy _fuzzy = new Fuzzy();
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult UploadFile(string file)
+        {
+            byte[] fileArr = new byte[0];
+            if (string.IsNullOrEmpty(file) == true)
+                return Json(new { message = "Файл пуст"});
+
+            var fl = file.Split(',')[1];
+            fileArr = Convert.FromBase64String(fl);
+
+            string tempPath = Path.GetTempFileName();
+            System.IO.File.WriteAllBytes(tempPath, fileArr);
+
+            Session[FILE_KEY] = tempPath;
+
+            string[] columns = ReadExcelFile(tempPath);
+
+            return Json(new { message = "Файл успешно загружен", columns });
+        }
+
+        /// <summary>
+        /// Работа с файлом
+        /// </summary>
+        /// <param name="fileName"></param>
+        private string[] ReadExcelFile(string fileName)
+        {
+            var app = new Application
+            {
+                DisplayAlerts = false,
+                ScreenUpdating = false,
+                IgnoreRemoteRequests = true
+            };
+
+            string[] columns;
+            try
+            {
+                var excelBook = app.Workbooks.Open(fileName);
+                var sheet = excelBook.Sheets.FirstOrDefault() as Worksheet;
+                var fullRange = sheet.UsedRange;
+                int firstRowIndex = fullRange.Row;
+                var firstColIndex = fullRange.Column;
+                var fullRangeValues = (object[,])fullRange.Value;
+                int lastRowIndex = fullRangeValues.GetLength(0);
+                var lastColumnIndex = fullRangeValues.GetLength(1);
+
+                
+                excelBook.Close();
+                app.Quit();
+
+                _logger.Info($"Строки с {firstRowIndex} по {lastRowIndex}. Колонки с {firstColIndex} по {lastColumnIndex}");
+
+                // Заполняем список колонок
+                columns = new string[lastColumnIndex];
+                for (int i = firstColIndex; i <= lastColumnIndex; i++)
+                    columns[i - firstColIndex] = fullRangeValues[firstRowIndex, i] as string;
+
+                Session[FIRST_ROW_INDEX_KEY] = firstRowIndex;
+                Session[LAST_ROW_INDEX_KEY] = lastRowIndex;
+                Session[FULL_RANGE_VALUES_KEY] = fullRangeValues;
+
+                return columns;
+            }
+            catch (Exception ex)
+            {
+                _logger.Error(ex);
+                throw;
+            }
+        }
+
+        /// <summary>
+        /// Первоначальная обработка файла
+        /// </summary>
+        /// <param name="columnIndex"></param>
+        /// <returns></returns>
+        public ActionResult ProcessFile(int columnIndex)
+        {
+            PrepareResult prepareResult = PrepareAutoCorrection(columnIndex + 1, fuzzyness, autoFuzzyness);
+            if (prepareResult == null)
+                return Json(new { message = "Не удалось обработать файл"});
+
+            return Json(new { message = "Файл успешно обработан", prepareResult });
+        }
+
+        /// <summary>
+        /// Выполнить автокоррекцию
+        /// </summary>
+        /// <param name="columnIndex"></param>
+        private PrepareResult PrepareAutoCorrection(int columnIndex, double fuzzyness, double autoCorrectionFuzzyness)
+        {
+            // Вычитываем все значения из выбранной колонки
+            int firstRowIndex = (int)Session[FIRST_ROW_INDEX_KEY];
+            int lastRowIndex = (int)Session[LAST_ROW_INDEX_KEY];
+            object[,] fullRangeValues = (object[,])Session[FULL_RANGE_VALUES_KEY];
+            Session[COLUMN_INDEX_KEY] = columnIndex;
+            
+            string[] values = new string[lastRowIndex - firstRowIndex];
+            for (int i = firstRowIndex + 1; i <= lastRowIndex; i++)
+            {
+                values[i - firstRowIndex - 1] = fullRangeValues[i, columnIndex] as string;
+            }
+
+            return _fuzzy.Prepare(values, fuzzyness, autoCorrectionFuzzyness);
+        }
+    }
+}
---
+++ Web/Scripts/App.js
@@ -0,0 +1,204 @@
+﻿'use strict';
+
+const logElement = document.querySelector('#log');
+
+$(document).ready(function () {
+    var dropZone = $('#dropZone');
+
+    if (typeof (window.FileReader) === 'undefined') {
+        dropZone.text('Не поддерживается браузером!');
+        dropZone.addClass('error');
+    }
+
+    dropZone[0].ondragover = function () {
+        dropZone.addClass('hover');
+        return false;
+    };
+
+    dropZone[0].ondragleave = function () {
+        dropZone.removeClass('hover');
+        return false;
+    };
+
+    dropZone[0].ondrop = function (event) {
+        event.preventDefault();
+        dropZone.removeClass('hover');
+        dropZone.addClass('drop');
+
+        const file = event.dataTransfer.files[0];
+        const reader = new FileReader();
+        reader.onload = onFileLoad;
+        reader.readAsDataURL(file);
+    };
+});
+
+function dropZoneOnClick() {
+    $('#fileInput').trigger('click');
+}
+
+function fileChange(e) {
+
+    var reader = new FileReader();
+    var files = e.dataTransfer ? e.dataTransfer.files : e.target.files;
+
+    reader.onload = onFileLoad;
+    reader.readAsDataURL(files[0]);
+}
+
+/**
+ * Загрузка файла
+ * @param {any} e
+ */
+function onFileLoad(e) {
+    const data = e.target.result;
+
+    $.ajax({
+        type: "POST",
+        url: "/Home/UploadFile",
+        data: { file: data },
+        success: function (res) {
+            AddLog(res.message);
+            if (res.columns)
+                InitColumns(res.columns);
+        },
+        error: function (q, w, e, r) {
+            console.log('error');
+            AddLog('Произошла ошика. Подробности в консоли');
+        }
+    });
+}
+
+/**
+ * Проинициализировать контрол с колонками
+ * @param {any} columns
+ */
+function InitColumns(columns) {
+    const columnSelect = document.querySelector('#column');
+    columnSelect.innerHTML = '';
+
+    columns.forEach((elem, ind) => {
+        const option = document.createElement('option');
+        option.innerHTML = elem;
+        option.value = ind;
+
+        columnSelect.appendChild(option);
+    });
+}
+
+/**
+ * 
+ */
+function ProcessFile() {
+    const columnIndex = document.querySelector('#column').selectedIndex;
+    if (columnIndex < 0) {
+        AddLog('Не выбрана колонка');
+        return;
+    }
+
+    $.ajax({
+        type: "POST",
+        url: "/Home/ProcessFile",
+        data: { columnIndex: columnIndex },
+        success: function (res) {
+            AddLog(res.message);
+            if (res.prepareResult) {
+                ShowVariants(res.prepareResult);
+                AddReplaceLog(res.ReplacementLog)
+            }
+        },
+        error: function (q, w, e, r) {
+            console.log('error');
+            AddLog('Произошла ошика. Подробности в консоли');
+        }
+    });
+}
+
+var variantIndex = 0;
+var possibleReplaces;
+
+/**
+ * 
+ * @param {any} prepareResult
+ */
+function ShowVariants(prepareResult) {
+
+    possibleReplaces = prepareResult.PossibleReplaces;
+
+    variantIndex = possibleReplaces.length - 1;
+
+    Next();
+}
+
+/**
+ * 
+ * @param {any} replacedValues
+ */
+function AddReplaceLog(replacedValues) {
+    AddLog('Произведена автозамена:');
+    replacedValues.forEach(elem => {
+        AddLog(elem);
+    });
+}
+
+/**
+ * 
+ */
+function DownloadFile() {
+
+}
+
+/**
+ * Выбор следующего набора возможных вариантов
+ */
+function Next() {
+
+    if (variantIndex < 0) {
+        return;
+    }
+
+    const variantsCount = document.querySelector('#variantsCount');
+    variantsCount.innerHTML = variantIndex + 1;
+
+    const variantsChoose = document.querySelector('#variantsChoose');
+    variantsChoose.innerHTML = '';
+
+    const variants = possibleReplaces[variantIndex--];
+
+    variants.forEach((elem, ind) => {
+        const option = document.createElement('option');
+        option.innerHTML = elem;
+        option.value = ind;
+
+        variantsChoose.appendChild(option);
+    });
+}
+
+/**
+ * Удалить выбранное название компании
+ */
+function Remove() {
+
+    $("#variantsChoose option:selected").remove();
+}
+
+
+/**
+ * Добавить названия компаний в базу
+ */
+function AddValue() {
+    const variantsChoose = document.querySelector('#variantsChoose');
+}
+
+
+/**
+ * Добавление лога
+ * @param {any} text
+ */
+function AddLog(text) {
+
+    const textLog = document.createElement('div');
+    textLog.classList.add('text-log');
+    textLog.innerHTML = text;
+
+    logElement.appendChild(textLog);
+}
